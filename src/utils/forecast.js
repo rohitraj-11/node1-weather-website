b9f7f2be1e37076cce2ea3d0e4bde86f0ed4b450
Your Name <you@example.com>
2020-08-04 09:30:33 +0530

    Added information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude,longitude,callback) =>{
         } else if (body.error) {
             callback('Unable to find location',undefined)
         } else {
-            callback(undefined,`${body.current.weather_descriptions[0]}. The temperature is ${body.current.temperature} fahrenheit and the humidity is ${body.current.humidity}.`)   
+            callback(undefined,`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} fahrenheit out. It feels like ${body.current.feelslike}. The humidity is ${body.current.humidity}.`)   
         }
     })
 }
